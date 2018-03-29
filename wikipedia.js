b530fdabf607f3b074dfac1628e51c4bf2839aa6
Brandon Murch <brandon.murch@gmail.com>
2018-03-29 18:16:13 +1100

    Reformat CSS to BEM

--- wikipedia.js
+++ wikipedia.js
@@ -1,19 +1,23 @@
+/*
+@description - The classic game of Simon, where the user has to recreate
+               the pattern that the computer creates.
+@input - based solely on clicking the GUI to create the correct pattern.
+@author - Brandon - Brandon.Murch@protonmail.com
+*/
+
+
 $(document).ready(function() {
-  let searchBar = $('#searchBar');
-  let searchBarInput = $('.searchBarInput');
-  $(searchBarInput).val('');
-  let searchBarBtn = $('.searchBarBtn');
-  let searchBarIcon = $('.searchBarIcon');
-  let clearSearch = $('.clearSearch');
   let isOpen = false;
-  let wikipediaResults = $('.wikipediaResults')
-  searchBarIcon.click(function(){
+  $('.searchBar__input').val(''); //Clears search field.
+
+  $('.button--searchBar').click(function(){
     if (isOpen == false){
       isOpen = true;
       open();
     }
   });
-  $('.searchBarInput').autocomplete({
+              //Jquery autocomplete, opens the drop-down menu.
+  $('.searchBar__input').autocomplete({
 	    source: function(request, response) {
 	        $.ajax({
 	            url: "http://en.wikipedia.org/w/api.php",
@@ -29,9 +33,10 @@ $(document).ready(function() {
 	        });
 	    }
 	});
-  searchBarBtn.click(function(event){
+
+  $('.button--submit').click(function(event){
     event.preventDefault();
-    let userInput = searchBarInput.val();
+    let userInput = $('.searchBar__input').val();
     if (userInput.length !== 0) {
       wikiSearch(userInput);
       blurAll();
@@ -40,34 +45,34 @@ $(document).ready(function() {
     }
   });
 
-  clearSearch.click(function(event){
-    searchBarInput.val('');
-    $('.wikipediaResults').empty();
+  $('.searchBar__clear').click(function(event){
+    $('.searchBar__input').val('');
+    $('.searchResults').empty();
     close();
   });
-
+                //Enter submits the query, ESC clears the search bar.
   document.onkeydown = function(event) {
     if (event.keyCode == 13) {
-      searchBarBtn.click();
+      $('.button--submit').click();
     } else if (event.keyCode == 27) {
-      clearSearch.click();
+      $('.searchBar__clear').click();
     }
   }
-
+                //opens the search bar by adding a class to searchBar.
   const open = () => {
-    searchBar.addClass('searchBarOpen');
-    searchBarBtn.css('z-index', '20');
+    $(".searchBar").addClass('searchBar--open');
+    $('.button--submit').css('z-index', '20');
     isOpen = true;
   }
-
+                //closes the search bar by removing a class to searchBar.
   const close = () => {
-    $(searchBar).removeClass('searchBarOpen');
-    $(searchBarBtn).css('z-index', '-10');
+    $(".searchBar").removeClass('searchBar--open');
+    $('.button--submit').css('z-index', '-10');
     isOpen = false;
-    searchBarInput.val('');
+    $('.searchBar__input').val('');
     blurAll();
   }
-
+                //un-focuses from the search bar.
   const blurAll = () => {
    var tmp = document.createElement('input');
    document.body.appendChild(tmp);
@@ -76,17 +81,20 @@ $(document).ready(function() {
   }
 
 });
+              //AJAX call to wikipedia, then placing the results in HTML.
 const wikiSearch = (input) => {
   $.get('https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch='
-   + prepareForLink(input),
-  function(data) {
+   + prepareForLink(input), (data) => {
     let html = '';
     $.each(data.query.pages, function(i, item) {
-      html += '<a href="https://en.wikipedia.org/?curid=' + item.pageid + '" target="_blank"><div class = "eachResult">';
-      html += '<h2 class = "wikiTitle">' + item.title + '</h2><p class = "wikiText">' + item.extract + '"</p>';
-      html += "</div></a>";
+      html += '<a class="searchResults__wikipediaLink" href="https://en.wikipedia.org/?curid='
+      + item.pageid + '" target="_blank"><div class = "searchResults__wikipediaPage layout--light ">'
+      + '<h2 class = "searchResults__wikipediaTitle searchResults__wikipediaText">' + item.title
+      + '</h2><p class = "searchResults__wikipediaText searchBar__text searchBar__text--wikipediaText">'
+      + item.extract + '"</p>'
+      +"</div></a>";
     });
-    $('.wikipediaResults').html(html);
+    $('.searchResults').html(html);
 }, 'jsonp');
 }
 
